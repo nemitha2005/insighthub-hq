7fd77af386822399f5e1ca7b611038ca7d4f9bce
nemitha2005 <nemithan05@gmail.com>
2025-05-13 21:19:35 +0530

    chore: change form logo svgs

--- src/components/authentication/sign-up-form.tsx
+++ src/components/authentication/sign-up-form.tsx
@@ -107,7 +107,7 @@ export function SignupForm() {
       }}
       className={'px-6 md:px-16 pb-6 py-8 gap-6 flex flex-col items-center justify-center'}
     >
-      <Image src={'/assets/icons/logo/aeroedit-icon.svg'} alt={'InsightHub'} width={80} height={80} />
+      <Image src={'/assets/icons/logo/insighthub-icon.svg'} alt={'InsightHub'} width={80} height={80} />
       <div className={'text-[30px] leading-[36px] font-medium tracking-[-0.6px] text-center'}>Create an account</div>
 
       <Button
